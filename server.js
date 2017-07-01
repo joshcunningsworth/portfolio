f88cea1f3f08ce78872dfc0ccc09181dfd314877
Josh Cunningham <Josh@Joshs-MacBook-Pro.local>
2017-07-01 01:42:24 -0400

    handlebar ting

--- server.js
+++ server.js
@@ -3,12 +3,18 @@ var app = express();
 var path = require('path');
 var HTTP_PORT = process.env.PORT || 8080;
 
-function onhttpstart(){
+app.engine(".hbs", exphbs({
+    extname: ".hbs",
+    defaultLayout: 'layout',
+}));
+app.set("view engine", ".hbs");
+
+function onhttpstart() {
     console.log("It's listening on " + HTTP_PORT);
 }
 
-app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname+"/test.html"));
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + "/test.html"));
 })
 
 //app.get('/index', function(req, res){
